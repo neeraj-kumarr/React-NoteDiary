901b44ab2cbad4eb9c00e5ddec1b8038539de5eb
Neeraj Kumar <neeraj.dsu@gmail.com>
2023-11-24 15:39:18 +0500

    Added express-validator for name, email and pass

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
+
 const router = express.Router();
 const User = require('../models/User')
 
-router.get('/', (req, res) => {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send("Hello Auth")
+router.post('/', [
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail().withMessage('Not a valid e-mail address'),
+    body('password').isLength({ min: 5 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.send({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user))
+        .catch(err => console.log(err))
+
+    // res.send(req.body)
 });
 
 module.exports = router;
