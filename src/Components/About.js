81a4c5c545bd20e8072b2e62cba4e0a03648ed40
Neeraj Kumar <neeraj.dsu@gmail.com>
2023-12-01 13:54:47 +0500

    useContext for displaying title

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,14 +2,10 @@ import React, { useContext, useEffect } from 'react'
 import noteContext from '../context/noteContext'
 
 const About = () => {
-    const a = useContext(noteContext)
-    useEffect(() => {
-        a.update();
-        // eslint-disable-next-line
-    }, [])
+
     return (
         <div>
-            Hello I am About {a.state.name} and I am in Class {a.state.class}
+            Hello I am About
         </div>
     )
 }
--- src/context/noteState.js
+++ src/context/noteState.js
@@ -2,23 +2,62 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Neeraj",
-        "class": "8A"
-    }
 
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": 'Neeraj Kumar',
-                'class': '8A CS'
-            })
-        }, 1000);
-    }
+    const notesInitial = [
+        {
+            "_id": "656339307c61e6c06471e46c",
+            "title": "Exercise",
+            "description": "walk for atleast 5 minutes",
+            "tag": "walk",
+            "date": "2023-11-26T12:25:20.026Z",
+            "__v": 0
+        },
+        {
+            "_id": "656739caec5dbd15980d921b",
+            "title": "Exercise",
+            "description": "walk for atleast 5 minutes",
+            "tag": "walk",
+            "date": "2023-11-29T13:16:58.194Z",
+            "__v": 0
+        },
+        {
+            "_id": "65686a43779fa0af201fbca0",
+            "title": "Run",
+            "description": "Run from the world",
+            "tag": "hello",
+            "date": "2023-11-30T10:56:03.130Z",
+            "__v": 0
+        },
+        {
+            "_id": "65686bc17bc5eb5d6b67a2c1",
+            "title": "Run",
+            "description": "Run from the world",
+            "tag": "hello",
+            "date": "2023-11-30T11:02:25.537Z",
+            "__v": 0
+        },
+        {
+            "_id": "65686cfcea0661f2cdafe2c7",
+            "title": "Run",
+            "description": "Run from the world",
+            "tag": "hello",
+            "date": "2023-11-30T11:07:40.318Z",
+            "__v": 0
+        },
+        {
+            "_id": "65686d01ea0661f2cdafe2c9",
+            "title": "Run2",
+            "description": "Run from the world",
+            "tag": "hello",
+            "date": "2023-11-30T11:07:45.691Z",
+            "__v": 0
+        }
+    ]
 
-    const [state, setState] = useState(s1);
+    const [notes, setNotes] = useState(notesInitial)
+
     return (
-        <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider value={{ notes, setNotes }}>
             {props.children}
         </NoteContext.Provider>
     )
